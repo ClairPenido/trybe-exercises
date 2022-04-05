bbee7c67ef3883bf68407e9f3f09a462ce84341b
Clair Penido <clairpenido@gmail.com>
2022-04-05 19:21:16 -0300

    mais um exercicio

--- 01- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ 01- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -5,6 +5,12 @@ test('Somatorio 4 + 5 é igual 9', () => {
   expect(sum(4, 5)).toBe(9);
 });
 
+// Teste se o retorno de sum(0, 0) é 0
 test('Somatorio 0 + 0 é igual 0', () => {
     expect(sum(0, 0)).toBe(0);
+  });
+
+// Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+test('Somatorio 4 + "5" é igual 9', () => {
+    expect(() => sum(4, '5')).toThrowError();
   });
