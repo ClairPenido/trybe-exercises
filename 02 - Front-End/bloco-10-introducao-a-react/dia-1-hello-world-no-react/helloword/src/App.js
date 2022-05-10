6e2dc04faf0d963c023f13f4f8cb96d2bff12b4c
Clair Penido <clairpenido@gmail.com>
2022-05-10 13:29:39 -0300

    exercicio de fixação

--- 02 - Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/helloword/src/App.js
+++ 02 - Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/helloword/src/App.js
@@ -1,13 +1,10 @@
 // App.js
 import React from 'react';
-import './App.css';
+import HelloWorld from './HelloWorld'
+
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>APP</h1>
-    </div>
-  );
+  return <HelloWorld />
 }
 
 export default App;
---
+++ 02 - Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/helloword/src/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from './App'
+
+class HelloWorld extends React.Component { // classe JS com o código react embutido
+    render() { // se quiser imprimir alguma coisa na tela
+        return <span> Hello, World! </span>
+    }
+}
+
+export default HelloWorld;
