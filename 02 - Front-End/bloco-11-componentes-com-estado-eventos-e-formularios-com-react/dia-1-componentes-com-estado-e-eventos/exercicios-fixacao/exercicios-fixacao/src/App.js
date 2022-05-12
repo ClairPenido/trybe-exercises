4063ec2a9a7ef171bce0f9c3bbb559cd4b49d3e5
Clair Penido <clairpenido@gmail.com>
2022-05-12 16:49:52 -0300

    aloo

--- 02 - Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/exercicios-fixacao/src/App.js
+++ 02 - Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/exercicios-fixacao/src/App.js
@@ -1,46 +1,44 @@
 import React from 'react';
-import '.App.css';
-
 
 class App extends React.Component { // classe dos componentes 
   constructor(){
     super();
-    this.state = {
-      numCliquesBotao1: 0, 
-      numCliquesBotao2: 0,
-      numCliquesBotao3: 0
-    }      
+
     this.buttonOne = this.buttonOne.bind(this); // para ter acesso as props
     this.buttonTwo = this.buttonTwo.bind(this);
     this.buttonThree = this.buttonThree.bind(this);
+  
+    this.state = {
+      numeroDeCliques1: 0, 
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    }      
+    
   }
-  buttonOne = () => {
+  buttonOne() {
     /* Passando uma callback à função setState, que recebe de parâmetros
     o estado anterior e as props do componente, você garante que as atualizações
     do estado acontecerão uma depois da outra! */
-    console.log('Clicou no botão um!');
-    this.setState((estadoAnterior, _props) => ({
+    this.setState((estadoAnterior) => ({
       numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
     }))
   }
-buttonTwo = () => {
-    console.log('Clicou no botão dois!');
-    this.setState((estadoAnterior, _props) => ({
+buttonTwo() {
+    this.setState((estadoAnterior) => ({
       numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
     }))
 }
-buttonThree = () => {
-  console.log('Clicou no botão três!');
-  this.setState((estadoAnterior, _props) => ({
+buttonThree() {
+  this.setState((estadoAnterior) => ({
     numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
   }))
 }
   render() {
     return (
     <div>
-    <button onClick={ this.buttonOne }>Botão 1 </button>
-    <button onClick={ this.buttonTwo }>Botão 2</button>
-    <button onClick={ this.buttonThree }>Botão 3</button>
+    <button onClick={ this.buttonOne }>Botão 1 | Numero de cliques: {this.state.numeroDeCliques1}</button>
+    <button onClick={ this.buttonTwo }>Botão 2 | Numero de cliques: {this.state.numeroDeCliques2}</button>
+    <button onClick={ this.buttonThree }>Botão 3 | Numero de cliques: {this.state.numeroDeCliques3}</button>
     </div>
   )};
 }
